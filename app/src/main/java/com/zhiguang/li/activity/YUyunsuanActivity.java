b6c9da8e07cc50a74af4244f0b8220a41e244b35
zhiguang.li <zhiguang.li@huanxi.fm>
2017-06-22 16:55:10 +0800

    与 运算

---
+++ app/src/main/java/com/zhiguang/li/activity/YUyunsuanActivity.java
@@ -0,0 +1,25 @@
+package com.zhiguang.li.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.zhiguang.li.R;
+
+public class YUyunsuanActivity extends AppCompatActivity {
+
+    private TextView text_yu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yuyunsuan);
+        text_yu = (TextView) findViewById(R.id.text_yu);
+
+        setView();
+    }
+
+    private void setView() {
+        text_yu.setText(""+ (15 & 15)+ "\n" + (15 & 1)+ "\n" + (15 & 2)+ "\n" + (15 & 4) + (15 & 8) + "\n" + (15 & 16));
+    }
+}
