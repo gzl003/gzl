9adc0e2f99eefa3e4d2e81829e62494f2cc688eb
zhiguang.li <zhiguang.li@huanxi.fm>
2017-11-24 15:40:55 +0800

    帧动画练习

--- app/src/main/java/com/zhiguang/li/activity/GifActivity.java
+++ app/src/main/java/com/zhiguang/li/activity/GifActivity.java
@@ -1,15 +1,33 @@
 package com.zhiguang.li.activity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.zhiguang.li.R;
 
 public class GifActivity extends AppCompatActivity {
 
+    private ImageView longmao;
+    private View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
+        longmao = (ImageView) findViewById(R.id.longmao);
+        startImageAnimation();
+
+    }
+
+    /**
+     * 开始图片动画
+     */
+    private void startImageAnimation() {
+        AnimationDrawable animationDrawable = (AnimationDrawable) getResources().getDrawable(R.drawable.anmain_longmao);
+        longmao.setImageDrawable(animationDrawable);
+        animationDrawable.start();
     }
 }
