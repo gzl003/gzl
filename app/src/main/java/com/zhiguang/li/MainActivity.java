0aeb28cd7752bc7f74024f5eada6cb0e2d2a0eda
zhiguang.li@huanxi.com <QAZqaz123>
2021-09-08 16:59:42 +0800

    更新gradle版本 设备信息测试

--- app/src/main/java/com/zhiguang/li/MainActivity.java
+++ app/src/main/java/com/zhiguang/li/MainActivity.java
@@ -53,6 +53,7 @@ import com.zhiguang.li.activity.PageIntedectorActivity;
 import com.zhiguang.li.activity.PaletteActivity;
 import com.zhiguang.li.activity.PasswordinputActivity;
 import com.zhiguang.li.activity.PathLineActivity;
+import com.zhiguang.li.activity.PipActivity;
 import com.zhiguang.li.activity.PoorVisionActivity;
 import com.zhiguang.li.activity.ProgressActivity;
 import com.zhiguang.li.activity.PtrRecycleViewActivity;
@@ -105,14 +106,14 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemClic
             , WebViewRefshActivity.class, SwichButtonActivity.class, RcycleViewImagetActivity.class, GreenDaoActivity.class, YUyunsuanActivity.class, SlidingCloseActivity.class, PoorVisionActivity.class
             , PaletteActivity.class, WatermarkActivity.class, QRCodeActivity.class, GalleryActivity.class, GalleryTwoActivity.class, RecyclerViewPageActivity.class, PageIntedectorActivity.class,
             X5WebView.class, ShareActivity.class, LzgSocketActivity.class, ScreenSwitchingActivity.class, ScrollScreenhotActivity.class, ObserverActivity.class, ThreadCycleActivity.class, TextViewEndActivity.class,
-            YkTopTabActivity.class
+            YkTopTabActivity.class, PipActivity.class
     };
     public String[] strings = {"RecyclerView", "点赞动画", "圆角图片", "分类", "选集", "AdapterViewFlipper", "gps"
             , "信息", "轮播图", "剧集", "drawer侧滑", "跟随动画", "map json", "destest", "贝塞尔曲线"
             , "视频录制", "google自家的刷新View", "服务测试", "webView测试", "加载框", "滚动的Activity", "RecyclerView 实现流式布局", "嵌套滚动", "CodeLayActivity", "TAB切换", "仿照微信左右切换", "TABlayout顶部切换", "闪光字体",
             "上下刷新的RecycleView", "图片和base64之间的转换", "SuperSwipeRefresh", "MediaPlayer测试", "gif图", "典藏馆", "密码输入框", "图片缩放", "侧滑删除item", "垂直进度条", "滑动的webView", "切换开关", "recycleImage",
             "GreenDao", "与 运算", "右滑关闭页面", "滚动视觉差", "Palette", "图片水印", "二维码", "画廊效果", "画廊效果2", "仿照viewpager的recyclerview", "PageIntedector", "x5浏览器", "分享", "Socket", "屏幕切换", "滚动截屏",
-            "观察者", "线程安全","TextView末尾添加符号","仿照优酷TabLayout"};
+            "观察者", "线程安全", "TextView末尾添加符号", "仿照优酷TabLayout", "画中画"};
     private List<MClassmate> mClassmates;
     private Banner banner;
 
---
+++ app/src/main/java/com/zhiguang/li/activity/PipActivity.java
@@ -0,0 +1,133 @@
+package com.zhiguang.li.activity;
+
+import android.app.PictureInPictureParams;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.util.Rational;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.zhiguang.li.R;
+
+/**
+ * @author :智光
+ * @date :2021/8/12 10:56
+ * @desc :
+ */
+public class PipActivity extends BaseActivity {
+    PictureInPictureParams.Builder pipBuild = new PictureInPictureParams.Builder();
+    Rational rational = new Rational(16, 9);
+    private ConstraintLayout layout;
+    private TextView textView;
+    private TextView textView8;
+    private TextView textView9;
+    private TextView textView10;
+
+//    private ConstraintSet constraintSet = new ConstraintSet();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pip_layout);
+        textView = findViewById(R.id.bottom_text);
+        textView8 = findViewById(R.id.textView8);
+        textView9 = findViewById(R.id.textView9);
+        textView10 = findViewById(R.id.textView10);
+
+
+        layout = findViewById(R.id.top_img);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                    enterPictureInPictureMode(pipBuild.build());
+
+                }
+            }
+        });
+//        constraintSet.clone(layout);
+//        setRequestedOrientation(Configuration.ORIENTATION_LANDSCAPE);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode);
+
+        if (isInPictureInPictureMode) {
+            textView.setVisibility(View.GONE);
+            textView.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(getBaseContext(), PipActivity.class));
+                }
+            }, 3000);
+        } else {
+            textView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            //竖屏
+            toSmall();
+        }
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //横屏
+            toFull();
+        }
+
+    }
+
+    private void toSmall() {
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(layout);
+//        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) layout.getLayoutParams();
+//        params.width = (int) getResources().getDisplayMetrics().xdpi;
+//        params.height = params.width * 9 / 16;
+//        layout.setLayoutParams(params);
+
+        Toast.makeText(this, "toSmall", Toast.LENGTH_SHORT).show();
+        constraintSet.connect(textView8.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 16);
+        constraintSet.connect(textView8.getId(), ConstraintSet.BOTTOM, textView9.getId(), ConstraintSet.TOP, 16);
+        constraintSet.connect(textView9.getId(), ConstraintSet.TOP, textView8.getId(), ConstraintSet.BOTTOM, 16);
+        constraintSet.connect(textView9.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 16);
+        constraintSet.clear(textView10.getId(), ConstraintSet.TOP);
+        constraintSet.connect(textView10.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 16);
+        constraintSet.applyTo(layout);
+    }
+
+    private void toFull() {
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(layout);
+//        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) layout.getLayoutParams();
+//        params.width = (int) getResources().getDisplayMetrics().xdpi;
+//        params.height = (int) getResources().getDisplayMetrics().ydpi;
+//        layout.setLayoutParams(params);
+
+        Toast.makeText(this, "toFull", Toast.LENGTH_SHORT).show();
+        constraintSet.connect(textView8.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 16);
+        constraintSet.connect(textView8.getId(), ConstraintSet.BOTTOM, textView9.getId(), ConstraintSet.TOP, 16);
+        constraintSet.connect(textView9.getId(), ConstraintSet.TOP, textView8.getId(), ConstraintSet.BOTTOM, 16);
+        constraintSet.connect(textView9.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 16);
+        constraintSet.connect(textView10.getId(), ConstraintSet.TOP, textView9.getId(), ConstraintSet.BOTTOM, 16);
+        constraintSet.connect(textView10.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 16);
+        int[] child = {textView8.getId(), textView9.getId(), textView10.getId()};
+        float[] weights = {1, 1, 1};
+        constraintSet.createVerticalChain(ConstraintSet.PARENT_ID, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, child, weights, ConstraintSet.VERTICAL);
+        constraintSet.applyTo(layout);
+    }
+}
